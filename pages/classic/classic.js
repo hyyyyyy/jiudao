dbef8350e685962b4e76d5ebfd4423ba91582de9
hyyyyyy <33621222+hyyyyyy@users.noreply.github.com>
2018-09-26 23:13:05 +0800

    2018 9/26
    试了一哈箭头函数

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -5,14 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    test:1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(this.data.test)
+    wx.request({
+      url: 'http://bl.7yue.pro/v1/classic/latest',
+      header:{
+        appkey:"UQ2OrhuKYFDWSsFa"
+      },
+      success:(res)=>{
+        console.log(res)
+        console.log(this.data.test)
+      }
+    })
   },
 
   /**

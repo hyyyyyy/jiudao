43133b3ccb419dc7978654796321bded2108241b
linsandong <402579219@qq.com>
2018-10-23 17:19:35 +0800

    2018 10.23
    组件修改

--- components/epsoide/index.js
+++ components/epsoide/index.js
@@ -4,17 +4,30 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    index:Number
+    index:{
+      type:String,
+      observer: function (newVal, oldVal, changedPath) {
+        let val = newVal < 10 ? '0' + newVal : newVal
+        this.setData({
+          _index: val
+        })
+      }
+    }
+    
   },
 
   /**
    * 组件的初始数据
    */
-  data: {
-    year:null,
-    month:null
+  data:{
+    year: 0,
+    month:'',
+    _index:''
   },
+  //组件生命周期函数
+  // attached:function(){
 
+  // }
   /**
    * 组件的方法列表
    */

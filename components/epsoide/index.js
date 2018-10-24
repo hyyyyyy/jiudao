52fc54d50582c409a953da0f355407170d232fe6
linsandong <402579219@qq.com>
2018-10-24 17:43:55 +0800

    2018 10/24
    组件添加和修改

--- components/epsoide/index.js
+++ components/epsoide/index.js
@@ -22,12 +22,19 @@ Component({
   data:{
     year: 0,
     month:'',
-    _index:''
+    _index:'',
+    months:['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月']
   },
   //组件生命周期函数
-  // attached:function(){
-
-  // }
+  attached:function(){
+    let data = new Date();
+    let year = data.getFullYear();
+    let month = data.getMonth()
+    this.setData({
+      year:year,
+      month:this.data.months[month]
+    })
+  },
   /**
    * 组件的方法列表
    */
